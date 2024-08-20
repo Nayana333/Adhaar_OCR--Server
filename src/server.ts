ee3d658e7ee7e492105a29ca9d5474af0bf3eaf5
Nayana333 <nrnayana1@gmail.com>
2024-08-20 13:27:52 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+ dotenv.config();
+
+const app = express();
+console.log(process.env.PORT || 5000);
+
+const port = process.env.PORT || 5000 ;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript Node Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
